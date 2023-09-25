68d409ab87c0f817076dcf35ad2cc7ff1901df94
FatmaElzahraa227 <70411867+FatmaElzahraa227@users.noreply.github.com>
2023-09-25 04:16:49 +0300

    done

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -7,37 +7,44 @@ import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
   styleUrls: ['./contactus.component.css'],
 })
 export class ContactusComponent {
-
   forum: any = 'newAlSiraj';
+  successMessage: string = '';
   @ViewChild('nameInput', { static: false }) name!: ElementRef;
   @ViewChild('phoneInput', { static: false }) phone!: ElementRef;
   @ViewChild('emailInput', { static: false }) email!: ElementRef;
   @ViewChild('messageInput', { static: false }) message!: ElementRef;
-  ngOnInit(): void{
+  ngOnInit(): void {
     console.log(this.forum);
-    
+    this.successMessage = '';
   }
 
   onRadioChange(event: any) {
     this.forum = event.target.value;
-    
   }
-
 
   sendMessage(e: Event) {
     e.preventDefault();
     const form = e.target as HTMLFormElement;
-  
-    // Add the radio button value to the form data
+
     const formData = new FormData(form);
     formData.append('forum', this.forum);
-  
-    // Send the form using emailjs
-    emailjs.sendForm('service_8jvmld7', 'template_xuygd9v', form, 'HqDOhulP275cKf-SI')
-      .then((result: EmailJSResponseStatus) => {
-        console.log(result);
-      }, (error) => {
-        console.log(error.text);
-      });
+
+    emailjs
+      .sendForm(
+        'service_8jvmld7',
+        'template_xuygd9v',
+        form,
+        'HqDOhulP275cKf-SI'
+      )
+      .then(
+        (result: EmailJSResponseStatus) => {
+          console.log(result);
+          form.reset();
+          this.successMessage='Message Sent Successfully'
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   }
 }
